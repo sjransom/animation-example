6027afb83bafb4e5f0ef0c7015bd9ee32d93122b
Rhiannon Monks <rhiannon.monks@sky.uk>
2024-09-26 15:37:56 +0100

    added all images

--- app/index.tsx
+++ app/index.tsx
@@ -3,26 +3,51 @@ import { Animated, View, StyleSheet, Dimensions, Image } from "react-native";
 
 const { width } = Dimensions.get("window");
 const CARD_WIDTH = 200;
-const NUM_CARDS = 4;
 
 const images = [
   {
     id: "1",
-    imageSource: require("../assets/Card_Image_Portrait_Left.png"),
+    imageSource: require("../assets/Card_Image_Portrait_1.png"),
   },
   {
     id: "2",
-    imageSource: require("../assets/Card_Image_Portrait_3.png"),
+    imageSource: require("../assets/Card_Image_Portrait_2.png"),
   },
   {
     id: "3",
-    imageSource: require("../assets/Card_Image_Portrait_5.png"),
+    imageSource: require("../assets/Card_Image_Portrait_3.png"),
   },
   {
     id: "4",
     imageSource: require("../assets/Card_Image_Portrait_4.png"),
   },
+  {
+    id: "5",
+    imageSource: require("../assets/Card_Image_Portrait_5.png"),
+  },
+  {
+    id: "6",
+    imageSource: require("../assets/Card_Image_Portrait_6.png"),
+  },
+  {
+    id: "7",
+    imageSource: require("../assets/Card_Image_Portrait_7.png"),
+  },
+  {
+    id: "8",
+    imageSource: require("../assets/Card_Image_Portrait_8.png"),
+  },
+  {
+    id: "9",
+    imageSource: require("../assets/Card_Image_Portrait_9.png"),
+  },
+  {
+    id: "10",
+    imageSource: require("../assets/Card_Image_Portrait_10.png"),
+  },
 ];
+
+const NUM_CARDS = images.length;
 
 const Index = () => {
   const topRow = useRef(new Animated.Value(0)).current;
